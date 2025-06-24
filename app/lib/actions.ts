24609fd7ca7fc1eb6bc52b34fab9c6760205759a
Fernando Ramos <fernando-ramos94@hotmail.com>
2025-06-24 00:07:53 -0300

    fixed unused error variable in actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,6 +59,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
+      error 
     };
   }
  
@@ -98,7 +99,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    return { message: 'Database Error: Failed to Update Invoice.', error };
   }
  
   revalidatePath('/dashboard/invoices');
